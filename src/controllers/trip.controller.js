4a434c3727c794750fd37194d020140877c65ec1
Mirajul <116744616+mirajul22@users.noreply.github.com>
2024-08-10 16:55:07 +0530

    api response changes

--- src/controllers/trip.controller.js
+++ src/controllers/trip.controller.js
@@ -1,5 +1,5 @@
-const data = require("../Specifications.json");
-const Realdata = require("../TripDetails.json");
+const data = require("../response_1723288694944.json");
+const Realdata = require("../response_1723288694950.json");
 
 const Specs = async (req, res) => {
   res.send(JSON.stringify(data));
